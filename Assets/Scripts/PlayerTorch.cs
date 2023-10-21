a16ef738edb3188d632df885f09edae3f9d36f0f
Jur15 <moralesjur@gmail.com>
2023-10-21 10:47:34 -0600

    Escena de Player Swap
    Cambio de antorcha
    Limpieza de código

---
+++ Assets/Scripts/PlayerTorch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerTorch : MonoBehaviour
+{
+    [SerializeField] public bool isCarrier;
+    private Torch torch;
+    [SerializeField] private bool player1 = true;
+
+    void Start()
+    {
+        torch = transform.GetComponentInChildren<Torch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ignore commands if not the carrier or game is stopped
+        if (!isCarrier || GameManager.Instance.IsPaused || GameManager.Instance.IsGameOver)
+            return;
+        if (player1 && Input.GetKeyDown(KeyCode.F))
+            torch.DoSlash();
+        else if (!player1 && Input.GetKeyDown(KeyCode.RightControl))
+            torch.DoSlash();
+    }
+
+    public void SwitchCarrier()
+    {
+        isCarrier = !isCarrier;
+        torch.gameObject.GetComponent<SpriteRenderer>().enabled = isCarrier;
+    }
+
+    public void DoTorchHit()
+    {
+        torch.DoHit();
+    }
+
+}
