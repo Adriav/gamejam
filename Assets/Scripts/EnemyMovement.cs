7db00a3aed834fc15315934c41d0d01d1661cfa6
IssaAC00 <algueracmp@gmail.com>
2023-10-21 14:22:32 -0600

    movement del enemigo

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 2f;
+    public float patrolDistance = 2f;
+    public float changeDirectionInterval = 5f;
+    private float initialPositionY;
+    private int moveDirection = 1;
+    private float timeSinceDirectionChange = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPositionY = transform.position.y;
+        timeSinceDirectionChange = Random.Range(0f, changeDirectionInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceDirectionChange += Time.deltaTime;
+        if (timeSinceDirectionChange >= changeDirectionInterval)
+        {
+            ChangeDirectionRandomly();
+            timeSinceDirectionChange = 0f;
+        }
+        transform.Translate(Vector2.up * moveDirection * moveSpeed * Time.deltaTime);
+        float distanceMoved = Mathf.Abs(transform.position.y - initialPositionY);
+        if (distanceMoved >= patrolDistance)
+        {
+            ChangeDirection();
+        }
+
+    }
+
+    private void ChangeDirection()
+    {
+        moveDirection *= -1;
+    }
+
+    private void ChangeDirectionRandomly()
+    {
+        moveDirection = Random.Range(0, changeDirectionInterval) == 0 ? -1 : 1;
+    }
+
+
+
+}
