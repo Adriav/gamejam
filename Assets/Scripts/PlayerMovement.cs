706622d833f756e3d52b09a47382765e55be9e79
Adrián Rivera Ávila <adriveravila64@gmail.com>
2023-10-20 22:57:36 -0600

    Player 1 & 2 movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  private float movementSpeed = 5f;
+  private Rigidbody2D rb;
+  private Vector2 movementInput;
+  public bool player2 = false;
+  // Start is called before the first frame update
+  void Awake()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (player2)
     {
-        
+      movementInput.x = Input.GetAxisRaw("HorizontalTorch");
+      movementInput.y = Input.GetAxisRaw("VerticalTorch");
+      movementInput.Normalize();
+
+      rb.velocity = movementInput * movementSpeed;
+    }
+    else
+    {
+      movementInput.x = Input.GetAxisRaw("Horizontal");
+      movementInput.y = Input.GetAxisRaw("Vertical");
+      movementInput.Normalize();
+  
+      rb.velocity = movementInput * movementSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 }
